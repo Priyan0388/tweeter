53d408212636eb675be58097e8f24166dd3dfd40
Priyan0388 <priyanthan.s@hotmail.com>
2019-06-13 20:51:42 +0000

    created tweet, with hover ability. no images

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,5 +1,17 @@
 $(document).ready(function() {
-    $("textarea").keypress(function(){
-      console.log(this)
+    $("textarea").keydown(function(){
+      var maxLength = 140;
+      var charLength = $(this).val().length;
+      $(this).siblings('.counter').html(maxLength - charLength);
+      console.log (maxLength - charLength);
+      if (charLength >= maxLength){
+        $(this).siblings('.counter').addClass("red");
+        console.log("You have reached limit!");
+      } 
+      if (charLength <= maxLength){
+        $(this).siblings('.counter').removeClass("red");
+      }
     });
-});
+  });
+
+  
