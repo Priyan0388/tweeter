7dea6d552ed228f6514fe97c47e3c34cf2dcb5cc
Priyan0388 <priyanthan.s@hotmail.com>
2019-06-14 21:53:39 +0000

    added function to retrieve tweets from JSON

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,55 +3,9 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+$( document ).ready(function() {
 
-const data = [
-  {
-  "user": {
-    "name": "Newton",
-    "avatars": {
-      "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-      "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-      "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-    },
-    "handle": "@SirIsaac"
-  },
-  "content": {
-    "text": "If I have seen further it is by standing on the shoulders of giants"
-  },
-  "created_at": 1461116232227
-},
-{
-  "user": {
-    "name": "Descartes",
-    "avatars": {
-      "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-      "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-      "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-    },
-    "handle": "@rd" },
-  "content": {
-    "text": "Je pense , donc je suis"
-  },
-  "created_at": 1461113959088
-},
-{
-  "user": {
-    "name": "Johann von Goethe",
-    "avatars": {
-      "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-      "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-      "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-    },
-    "handle": "@johann49"
-  },
-  "content": {
-    "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-  },
-  "created_at": 1461113796368
-}
-];
-
-function renderTweets() {
+function renderTweets(data) {
   $tweetContainer = $("#tweet-container")
 
   data.forEach(function (tweet) {
@@ -78,7 +32,7 @@ function createTweetElement(tweetObj) {
 
   return `<article>
       <header>
-        <img src="https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png">
+        <img src="${tweetObj.user.avatars.small}">
         <h2>${tweetObj.user.name}</h2>
         <p>${tweetObj.user.handle}<p>
       </header>
@@ -95,4 +49,29 @@ function createTweetElement(tweetObj) {
   `
 }
 
-renderTweets();
+$(function() {
+  var form = $("#tweetForm");
+  form.submit(function (event) {
+    event.preventDefault();
+    var data = $(this).serialize();
+    $.post("/tweets", data)
+  });
+});
+
+function getTweets(){
+console.log("getting tweets:")
+/ $.ajax({
+    url:"/tweets",
+    type: "Get",
+    dataType: "json",
+    success: function(tweets){
+      console.log(tweets)
+      renderTweets(tweets);
+    }
+  })
+ 
+}
+
+getTweets()
+
+});
