33eefa0f85ab6631367766e8b1d6e8921e0b0a74
Priyan0388 <priyanthan.s@hotmail.com>
2019-06-14 19:20:17 +0000

    added function to append and display tweets

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,3 +4,95 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+const data = [
+  {
+  "user": {
+    "name": "Newton",
+    "avatars": {
+      "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+      "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+      "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+    },
+    "handle": "@SirIsaac"
+  },
+  "content": {
+    "text": "If I have seen further it is by standing on the shoulders of giants"
+  },
+  "created_at": 1461116232227
+},
+{
+  "user": {
+    "name": "Descartes",
+    "avatars": {
+      "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+      "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+      "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+    },
+    "handle": "@rd" },
+  "content": {
+    "text": "Je pense , donc je suis"
+  },
+  "created_at": 1461113959088
+},
+{
+  "user": {
+    "name": "Johann von Goethe",
+    "avatars": {
+      "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+      "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+      "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+    },
+    "handle": "@johann49"
+  },
+  "content": {
+    "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+  },
+  "created_at": 1461113796368
+}
+];
+
+function renderTweets() {
+  $tweetContainer = $("#tweet-container")
+
+  data.forEach(function (tweet) {
+    tweet.created_at = dateTime(tweet.created_at)
+    const $elm = $(createTweetElement(tweet))
+    $tweetContainer.prepend($elm)
+  })
+}
+
+function dateTime(timestamp){
+ var a = new Date(timestamp * 1000);
+ var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+ var year = a.getFullYear();
+ var month = months[a.getMonth()];
+ var date = a.getDate();
+ var hour = a.getHours();
+ var min = a.getMinutes();
+ var sec = a.getSeconds();
+ var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec ;
+ return time;
+}
+
+function createTweetElement(tweetObj) {
+
+  return `<article>
+      <header>
+        <img src="https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png">
+        <h2>${tweetObj.user.name}</h2>
+        <p>${tweetObj.user.handle}<p>
+      </header>
+      <p>${tweetObj.content.text}</p>
+      <footer>
+        <p>${tweetObj.created_at}</p>
+        <span class="icons">
+          <i class="fab fa-font-awesome-flag"></i>
+          <i class="fas fa-retweet"></i>
+          <i class="fas fa-heart"></i>
+      </span>
+      </footer>
+    </article>
+  `
+}
+
+renderTweets();
